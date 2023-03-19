bb295617f045ddbed02c8c076b6d60627e808e0e
yehaochuan <v_yhcye@tencent.com>
2023-03-19 18:41:22 +0800

    11

--- src/view/myNav/config/index.js
+++ src/view/myNav/config/index.js
@@ -6,13 +6,18 @@ export const barList = [
         name: 'JS',
         children: [
             { name: '近代js教程', Icon: jsSvg, intro: '近代js教程', url: 'https://zh.javascript.info/' },
-            { name: 'ES6', intro: 'ES6', type: 'text' url: 'https://es6.ruanyifeng.com/' },
+            { name: 'ES6', intro: 'ES6', type: 'text', url: 'https://es6.ruanyifeng.com/' },
         ]
     },
     {
         name: 'CSS',
         children: [
-            { name: 'Css Tricks', Icon: cssSvg, intro: '汇集了大量的CSS动画效果！', url: 'https://lhammer.cn/You-need-to-know-css/#/zh-cn/' },
+            {
+                name: 'Css Tricks',
+                Icon: cssSvg,
+                intro: '汇集了大量的CSS动画效果',
+                url: 'https://lhammer.cn/You-need-to-know-css/#/zh-cn/'
+            },
         ]
     },
     { name: '框架' },
